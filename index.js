04b65cf9e87b4af796497174bcad9ee63eeb1d5e
TAIWO KOLAWOLE <kolawole.taiwo@andela.com>
2019-08-27 08:36:29 +0100

    add instruction for publish message

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-console.log("Introduction to javascript");
-console.log("I am going to this one now");
+const messageField = document.getElementById('message');
+const publishButton = document.getElementById('publish-btn');
+const screen = document.getElementById('screen');
+
+// Objective of the program:
+// As a user, I want to enter any text in the input text field
+// As a user, When I click on the publish button,
+// I should see the message I typed display on the screen above
+// the input field.
+
+// Complete the remaining part of this app by following the steps below
+
+// Add event Listener here
+// https://www.w3schools.com/js/js_htmldom_eventlistener.asp
+
+// Inside the publish button event handler function
+// get the the value of message field
+// https://www.w3schools.com/jsref/prop_text_value.asp
+
+// Clear the inner html of the screen 
+// Create a new text node with message field and append it to screen
+// https://www.w3schools.com/js/js_htmldom_nodes.asp
